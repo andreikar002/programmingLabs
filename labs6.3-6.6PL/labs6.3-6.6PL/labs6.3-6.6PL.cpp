87637dcb15dfec3abb93ef9982faefaf688cdc4c
andreikar002 <90751630+andreikar002@users.noreply.github.com>
2022-02-04 17:10:11 +0300

    labs

---
+++ labs6.3-6.6PL/labs6.3-6.6PL/labs6.3-6.6PL.cpp
@@ -0,0 +1,129 @@
+﻿#include <iostream>
+#include <queue>
+#include <set>
+#include <list>
+#include <string>
+#include <map>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+
+
+
+//functions name:
+void proc6_3();
+void proc6_4();
+void proc6_5();
+void proc6_6();
+void menu();
+
+
+
+
+int main()
+{
+    while (1) {
+        menu();
+    }
+}
+
+
+
+//functions:
+void menu() {
+    cout << "\nWhat lab?(if you want to exit print 0)";
+    int varios; cin >> varios;
+    if (varios == 3)
+        proc6_3();
+    else if (varios == 4)
+        proc6_4();
+    else if (varios == 5)
+        proc6_5();
+    else if (varios == 6)
+        proc6_6();
+    else if (varios == 0)
+        exit(0);
+    else
+        cout << "wrong number";
+}
+void proc6_3() {
+    int n; cin >> n;
+    deque<int> x2, x3, x5;
+    x2.push_back(2);
+    x3.push_back(3);
+    x5.push_back(5);
+    for (int i = 0; i < n; i++) {
+        int min_n =  min(x2.front(), min(x3.front(), x5.front()));
+        cout << min_n << " ";
+        x2.push_back(min_n * 2);
+        x3.push_back(min_n * 3);
+        x5.push_back(min_n * 5);
+        if (x2.front() == min_n)
+            x2.pop_front();
+        if (x3.front() == min_n)
+            x3.pop_front();
+        if (x5.front() == min_n)
+            x5.pop_front();
+    }
+}
+
+void proc6_4() {
+    int n; cin >> n;
+    set<int> attempts;
+    int mas[2000]; // u can change number if u have more students
+    for (int i = 0; i < n; i++) {
+        int j; cin >> j;
+        if (attempts.count(j)) {
+            cin >> j;
+        }
+        else {
+            int newJ; cin >> newJ;
+            mas[j] = newJ;
+            attempts.insert(j);
+        }
+    }
+    for (auto i : attempts)
+        cout << i << " " << mas[i] << " \n";
+}
+
+void proc6_5() {
+    int n; cin >> n;
+    list<pair<int, string>> mas;
+    for (int i = 0; i < n; i++) {
+        int j; cin >> j;
+        string surname; cin >> surname;
+        mas.push_back(make_pair(j, surname));
+    }
+    for (auto i : mas)
+        cout << i.first << " " << i.second << "\n";
+}
+
+void proc6_6() {
+    ifstream in("text.txt");//reading from file because it's more comfortable
+    string text = "", str;
+    while (getline(in, str))
+        text += str;
+    map<string, int> res;
+    string slov = "";
+    int sum = 0;
+    for (int i = 0; i < text.length(); i++) {
+        if (text[i] != ' ') {
+            slov += text[i];
+        }
+        else {
+            res[slov]++;
+            slov = "";
+            sum++;
+        }
+        if (i == text.length() - 1) {
+            res[slov]++;
+            slov = "";
+            sum++;
+        }
+    }
+    for (auto it = res.cbegin(); it != res.cend(); it++) {
+        cout << it->first << " " << double(it->second) / double(sum) * 100.0 << "% \n";
+    }
+}
---
+++ labsPL3/labsPL3/labsPL3.cpp
@@ -0,0 +1,201 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+
+// functions name:
+int* Sort1(int mas[1001], int n);
+void Sort2(char mas[1001], int n, char* res);
+void Sort3(int* mas, int f_part, int l_part);
+void varios1();
+void varios2();
+void varios3();
+int lab3_1();
+int sum_nums(int a);
+bool comp1(int a, int b);
+void Sort3_2_1(int* mas, int n);
+bool comp2(int a, int b);
+void Sort3_2_2(int* mas, int n);
+int lab3_2();
+
+
+
+
+int main()
+{
+	cout << "chose lab(1 or 2)";
+	int v; cin >> v;
+	if (v == 1)
+		v = lab3_1();
+	else if (v == 2)
+		v = lab3_2();
+}
+
+
+
+
+//functions:
+int* Sort1(int mas[1001], int n) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n - 1; j++) {
+			if (mas[j] > mas[j + 1])
+				swap(mas[j], mas[j + 1]);
+		}
+	}
+	return mas;
+}
+
+
+void Sort2(char mas[1001], int n, char* res) {
+	int counting[26];
+	for (int i = 0; i < 26; i++)
+		counting[i] = 0;
+	for (int i = 0; i < n; i++) {
+		counting[int(mas[i]) - 97]++;
+	}
+	int tek = 0;
+	for (int i = 0; i < 26; i++) {
+		for (int j = 0; j < counting[i]; j++, tek++) {
+			res[tek] = char(i + 97);
+		}
+	}
+}
+
+void Sort3(int* mas, int f_part, int l_part) {
+	if (l_part > f_part) {
+		Sort3(mas, f_part, (f_part + l_part) / 2);
+		Sort3(mas, (f_part + l_part) / 2 + 1, l_part);
+		int res[1001];
+		int mid = (f_part + l_part) / 2;
+		int part1 = f_part, part2 = mid + 1, tek = f_part;
+		for (int i = f_part; i <= l_part; i++) {
+			if ((part1 <= mid) and (mas[part1] < mas[part2] or part2 > l_part)) {
+				res[i] = mas[part1];
+				part1++;
+			}
+			else {
+				res[i] = mas[part2];
+				part2++;
+			}
+		}
+		for (int i = f_part; i <= l_part; i++)
+			mas[i] = res[i];
+	}
+}
+
+void varios1() {
+	int mas[1001];
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> mas[i];
+	int* res = Sort1(mas, n);
+	for (int i = 0; i < n; i++) {
+		cout << res[i] << " ";
+	}
+}
+
+void varios2() {
+	char mas[1001];
+	int n;
+	char res[1001];
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> mas[i];
+	Sort2(mas, n, res);
+	for (int i = 0; i < n; i++) {
+		cout << res[i] << " ";
+	}
+}
+
+void varios3() {
+	int mas1[1001];
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> mas1[i];
+	Sort3(mas1, 0, n - 1);
+	for (int i = 0; i < n; i++) {
+		cout << mas1[i] << " ";
+	}
+}
+
+int lab3_1() {
+	while (1) {
+		int v; cin >> v;
+		if (v == 1)
+			varios1();
+		else if (v == 2)
+			varios2();
+		else if (v == 3)
+			varios3();
+		else
+			return 0;
+	}
+}
+
+int sum_nums(int a) {
+	int c = 0, res1 = 0, res2 = 0;
+	while (a > 0) {
+		if (c % 2 == 0)
+			res1 += a % 10;
+		else
+			res2 += a % 10;
+		c++;
+		a /= 10;
+	}
+	if (c % 2 == 1)
+		return res2;
+	else
+		return res1;
+}
+
+bool comp1(int a, int b) {
+	return sum_nums(a) < sum_nums(b);
+}
+
+void Sort3_2_1(int* mas, int n) {
+	sort(mas, mas + n, comp1);
+}
+
+bool comp2(int a, int b) {
+	if (a % 10 == b % 10)
+		return a > b;
+	return a % 10 < b % 10;
+}
+
+void Sort3_2_2(int* mas, int n) {
+	sort(mas, mas + n, comp2);
+}
+
+int lab3_2() {
+	int n;
+	int* mas = new int;
+	while (1) {
+		cout << "\n1: input mas\n2: output mas\n3: sort1\n4: sort2\n5: exit";
+		int v; cin >> v;
+		if (v == 1) {
+			cin >> n;
+			mas = new int[n];
+			for (int i = 0; i < n; i++)
+				cin >> mas[i];
+		}
+		if (v == 2) {
+			for (int i = 0; i < n; i++) {
+				cout << mas[i] << " ";
+			}
+		}
+		if (v == 3) {
+			Sort3_2_1(mas, n);
+		}
+		if (v == 4) {
+			Sort3_2_2(mas, n);
+		}
+		if (v == 5) {
+			cout << "exit";
+			return 0;
+		}
+	}
+}
